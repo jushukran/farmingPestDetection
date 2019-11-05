28327d28611ee49f2f92d9f823aa1aee4377937b
jushukran <shukran@students.uonbi.ac.ke>
2019-11-05 18:59:32 +0300

    registered admins

--- FPD/admin.py
+++ FPD/admin.py
@@ -1,6 +1,24 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Pest, Crop, Farmer, Farm, History, ControlMeasure, Solution
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Pest)
+admin.site.register(Crop)
+admin.site.register(Farmer)
+admin.site.register(Farm)
+admin.site.register(History)
+admin.site.register(ControlMeasure)
+
+class SolutionInLine(admin.StackedInline) :
+
+    model = Solution
+
+class ControlMeasureAdmin(admin.ModelAdmin) :
+    inlines = [
+        SolutionInLine
+    ]
+
+admin.site.register(ControlMeasureAdmin)
+
+
+
